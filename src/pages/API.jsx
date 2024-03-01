114091611ec48a9a7affa632e362b2acacbf2926
almario12345 <almarioelopre1@gmail.com>
2024-03-02 02:28:27 +0800

    8th

--- src/pages/API.jsx
+++ src/pages/API.jsx
@@ -69,6 +69,7 @@ const API = () => {
       </Body>
       <Navbar />
     </div>
+    
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Row, Col, Button } from 'react-bootstrap'; // Importing only necessary components from react-bootstrap
-import jwtDecode from 'jwt-decode'; // Corrected import statement
+import { Row, Col, Button } from 'react-bootstrap';
+import jwtDecode from 'jwt-decode'; 
 import { Body } from '../components/Body/Body';
 import "./styles.css";
 import { Title } from '../components/Title/Title';
